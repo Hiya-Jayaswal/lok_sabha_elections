263ea9bfd5974b8ab686894fc18fc126c37b6bdc
Hiya-Jayaswal <hiya.jayaswal@gmail.com>
2024-06-30 22:19:51 +0530

    initial commit

--- 00.py
+++ 00.py
@@ -1,3 +1,13 @@
-import bs4 as beautifulsoup
 import pandas as pd
+import bs4 as BeautifulSoup
 import requests
+
+print("hello")
+
+url = "https://results.eci.gov.in/PcResultGenJune2024/index.htm"
+
+respone = requests.get(url)
+
+my_data = []
+
+soup = BeautifulSoup(html_doc, 'html.parser')
